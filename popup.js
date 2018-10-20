031e9a14ecc743a770646fa699123d873904a07c
goorm <hmu332233@gmail.com>
2018-10-21 02:07:49 +0900

    [Add] FeatureSelector class

--- popup.js
+++ popup.js
@@ -1,3 +1,54 @@
+class FeatureSelector {
+  constructor(selectedIndex = 0) {
+    this.$featureButtons = document.querySelectorAll('.features__button');
+
+    this.selectedIndex = selectedIndex;
+    this.featureLength = this.$featureButtons.length;
+  }
+
+  increaseIndex() {
+    let nextIndex = this.selectedIndex + 1;
+    if (nextIndex >= this.featureLength) {
+      nextIndex = 0;
+    }
+
+    this.selectedIndex = nextIndex;
+  }
+
+  decreaseIndex() {
+    let nextIndex = this.selectedIndex - 1;
+    if (nextIndex < 0) {
+      nextIndex = this.featureLength - 1;
+    }
+
+    this.selectedIndex = nextIndex;
+  }
+  selectNextFeature() {
+    this.removeSelected();
+    this.increaseIndex();
+    this.addSelected();
+  }
+
+  selectPrevFeature() {
+    this.removeSelected();
+    this.decreaseIndex();
+    this.addSelected();
+  }
+
+  addSelected(selectedIndex = this.selectedIndex) {
+    this.$featureButtons[selectedIndex].classList.add('selected');
+  }
+
+  removeSelected() {
+    const $selectedFeatureButton = document.querySelector('.features__button.selected');
+    $selectedFeatureButton.classList.remove('selected');
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  window.a = new FeatureSelector();
+  
+
   
 });
